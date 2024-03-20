efcf97a363ddb3ada3ed6d1b73691d71b3ac31a5
donghyun960215 <tlzma789@naver.com>
2024-03-20 12:50:44 +0900

    기본적은 폴더 구조 및 리액트 라우터 설정 (2024.03.20 점심시간)

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom'
+import LogIn from '@pages/Login'
+import SigUp from '@pages/SignUp'
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return <Switch>
+    <Redirect exact path='/' to="/login"></Redirect>
+    <Route path="/login" component={LogIn}></Route>
+    <Route path="/sigup" component={SigUp}></Route>
+  </Switch>
 };
 
 export default App;
