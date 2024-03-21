1a6e34dc5187e0a24c0de6c1534d442246f9e2e9
donghyun960215 <tlzma789@naver.com>
2024-03-21 22:54:30 +0900

    퇴근 후 공부(24.03.21)

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom'
-import LogIn from '@pages/Login'
-import SigUp from '@pages/SignUp'
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const LogIn = loadable(() => import('@pages/Login'));
+const SigUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <Switch>
-    <Redirect exact path='/' to="/login"></Redirect>
-    <Route path="/login" component={LogIn}></Route>
-    <Route path="/sigup" component={SigUp}></Route>
-  </Switch>
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login"></Redirect>
+      <Route path="/login" component={LogIn}></Route>
+      <Route path="/sigup" component={SigUp}></Route>
+    </Switch>
+  );
 };
 
 export default App;
--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,9 +1,84 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Form, Label, Input, Header, LinkContainer, Button, Error } from './styles';
+import useInupt from '@hooks/useInupt';
 
 const SigUp = () => {
+  const [email, onChangeEmail] = useInupt('');
+  const [nickname, onChangeNickname] = useInupt('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value != passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value != password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError) {
+        console.log('서버로 넘기기', email, nickname, password, passwordCheck);
+      }
+    },
+    [email, nickname, password, passwordCheck, mismatchError],
+  );
+
   return (
-    <div>
-      회원가입
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/* {signUpError && <Error>{signUpError}</Error>} */}
+          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        {/* <Link to="/login">로그인 하러가기</Link> */}
+      </LinkContainer>
     </div>
   );
 };
